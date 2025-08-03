d60952305a2a969ed97aa0447d28e5084a1133a9
Vishal Sharma <vishal.en.work@gmail.com>
2025-08-03 15:29:59 +0530

    Initial Commit

---
+++ GUI/GridLayout & Checkbuttons.py
@@ -0,0 +1,56 @@
+from tkinter import *
+
+from sqlalchemy import column
+
+root = Tk()
+root.geometry("600x400")
+root.title("My Travels")
+
+# Creating a label widget
+Label(root, text="Welcome to My Travels", font="comicsansms 13 bold", pady=15).grid(row=0, column=3)
+
+# Creating a button widget
+name = Label(root, text="Name")
+phone = Label(root, text="Phone")
+email = Label(root, text="Email")
+gender = Label(root, text="Gender")
+payment_mode = Label(root, text="Payment Mode")
+
+# Pack is used to show the widgets on screen
+name.grid(row= 1, column=2)
+phone.grid(row= 2, column=2)
+email.grid(row= 3, column=2)
+gender.grid(row= 4, column=2)
+payment_mode.grid(row= 5, column=2)
+
+# Variable classes in tkinter
+name_value = StringVar()    # This is a string type variable class
+phone_value = StringVar()
+email_value = StringVar()
+gender_value = StringVar()
+payment_mode_value = StringVar()
+food_services_value = IntVar()   # This is an integer type variable class
+
+# Creating entry field widgets
+name_entry = Entry(root, textvariable=name_value)
+phone_entry = Entry(root, textvariable=phone_value)
+email_entry = Entry(root, textvariable=email_value)
+gender_entry = Entry(root, textvariable=gender_value)
+payment_mode_entry = Entry(root, textvariable=payment_mode_value)
+
+# Packing the entries
+name_entry.grid(row=1, column=3)
+phone_entry.grid(row=2, column=3)
+email_entry.grid(row=3, column=3)
+gender_entry.grid(row=4, column=3)
+payment_mode_entry.grid(row=5, column=3)
+
+# Checkbox and Radio Button
+food_service = Checkbutton(text="Want to prebook your meals?", variable=food_services_value)   # The value of the checkbox is stored in the variable food_services_value which is an integer. If the checkbox is checked then the value will be 1 else 0 respectively
+food_service.grid(row=6, column=3)
+
+# Submit button
+Button(text="Submit", command=lambda: print(f"The name of the person is {name_value.get()},\nthe phone number is {phone_value.get()},\nthe email address is {email_value.get()},\nthe gender is {gender_value.get()},\nthe payment mode is {payment_mode_value.get()},\nand the food services are {food_services_value.get()}")).grid(row=7, column=3)     # Lambda function is used to pass arguments to function without calling it.
+
+
+root.mainloop()
--- GUI/Widget & Grid Layout.py
+++ GUI/Widget & Grid Layout.py
@@ -6,5 +6,32 @@ root.geometry("655x335")
 user = Label(root, text="Username")
 password = Label(root, text="Password")
 
+def getvals():
+    print(f"{uservalue.get()}, {passvalue.get()}")    # f-string is used to format the output of the function getvals
+
+# Grid is used to place the widgets at specific positions on screen
+
+user.grid()   # row and column are optional parameters in grid method
+password.grid(row=1)
+
+# Variable Classes:
+'''
+BooleanVar - stores boolean values
+DoubleVar - stores double values
+IntVar - stores integer values
+StringVar - stores string values
+'''
+
+uservalue = StringVar()    # StringVar class object created to store user value entered by user
+passvalue = StringVar()
+
+userentry = Entry(root, textvariable= uservalue)    # Entry widget takes variable as an argument which will be stored in uservalue variable
+passentry = Entry(root, textvariable= passvalue)    # Entry widget takes variable as an argument which will be stored in passvalue variable
+
+userentry.grid(row=0, column=1)
+passentry.grid(row=1, column=1)
+
+
+btn = Button(text="Submit", command=getvals, bg="red" ).grid(column=1)
 
 root.mainloop()
